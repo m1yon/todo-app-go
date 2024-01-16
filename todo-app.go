a9ae90d9bf4764c50bdb98dc73b795a6a2de029e
Michael Lyon <mlyon360@gmail.com>
2024-01-15 18:55:31 -0700

    add basic template

--- todo-app.go
+++ todo-app.go
@@ -2,14 +2,36 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+type Todo struct {
+	Description string
+	Done        bool
+}
+
+type MainPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+
 func main() {
+	t := template.Must(template.ParseFiles("templates/index.tmpl"))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello there!!")
+		err := t.Execute(w, MainPageData{PageTitle: "Todo App",
+			Todos: []Todo{
+				{Description: "Go to store"},
+				{Description: "Clean windows"},
+				{Description: "Pay taxes", Done: true},
+			}})
+
+		if err != nil {
+			fmt.Println(err)
+		}
 	})
 
-	fmt.Println("🚀 App running on port 80")
+	fmt.Println("🚀 App running on port http://localhost:80")
 	http.ListenAndServe(":80", nil)
 }
